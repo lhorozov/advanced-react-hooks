97bea8c702c59c136399e75a8178d3de99f58cfc
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-01-11 22:41:46 +0200

    useReducer: simple Counter-extra credit-2

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -5,12 +5,14 @@ import * as React from 'react'
 
 function Counter({initialCount = 0, step = 1}) {
   const countReducer = (state, newState) => {
-    return state + newState
+    return newState
   }
-  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
 
-  const increment = () => changeCount(step)
-  return <button onClick={increment}>{count}</button>
+  const increment = () => setState({count: state.count + step})
+  return <button onClick={increment}>{state.count}</button>
 }
 
 function App() {
