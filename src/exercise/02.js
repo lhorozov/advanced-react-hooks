9f908b11bb7319fcac3e00f0c835a2343fbdebdf
Lyuben Horozov <lyuben.horozov@mentormate.com>
2022-01-13 09:46:56 +0200

    useCallback: custom hooks-extra credit-3-2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -10,6 +10,27 @@ import {
   PokemonErrorBoundary,
 } from '../pokemon'
 
+function useSafeDispatch(dispatch) {
+  const mountedRef = React.useRef(false)
+
+  React.useEffect(() => {
+    mountedRef.current = true
+
+    return () => {
+      mountedRef.current = false
+    }
+  }, [])
+
+  return React.useCallback(
+    (...args) => {
+      if (mountedRef.current) {
+        dispatch(...args)
+      }
+    },
+    [dispatch],
+  )
+}
+
 function asyncReducer(state, action) {
   switch (action.type) {
     case 'pending': {
@@ -35,21 +56,7 @@ function useAsync(initialState) {
     ...initialState,
   })
 
-  const mountedRef = React.useRef(false)
-
-  React.useEffect(() => {
-    mountedRef.current = true
-
-    return () => {
-      mountedRef.current = false
-    }
-  }, [])
-
-  const dispatch = React.useCallback((...args) => {
-    if (mountedRef.current) {
-      unsafeDispatch(...args)
-    }
-  }, [])
+  const dispatch = useSafeDispatch(unsafeDispatch)
 
   const run = React.useCallback(promise => {
     dispatch({type: 'pending'})
